d8b6714921242249eb64ed6aad6157c5ac091aa3
3mma-n <148897839+3mma-n@users.noreply.github.com>
2025-02-11 16:43:06 +0000

    2/11

--- src/main/java/com/example/project/WordScramble/WordScramble.java
+++ src/main/java/com/example/project/WordScramble/WordScramble.java
@@ -1,36 +1,56 @@
 package com.example.project.WordScramble;
 import java.util.ArrayList;
 public class WordScramble {
-  /** Scrambles a given word.
-   *
-   *  @param word  the word to be scrambled
-   *  @return  the scrambled word (possibly equal to word)
-   *
-   *  Precondition: word is either an empty string or contains only uppercase letters.
-   *  Postcondition: the string returned was created from word as follows:
-   *  - the word was scrambled, beginning at the first letter and continuing from left to right
-   *  - two consecutive letters consisting of "A" followed by a letter that was not "A" were swapped
-   *  - letters were swapped at most once
-   */
-  public static String scrambleWord(String word) {
-      /* to be implemented in part (a) */
-      return "";
-  }
+    /** Scrambles a given word.
+     *
+     *  @param word  the word to be scrambled
+     *  @return  the scrambled word (possibly equal to word)
+     *
+     *  Precondition: word is either an empty string or contains only uppercase letters.
+     *  Postcondition: the string returned was created from word as follows:
+     *  - the word was scrambled, beginning at the first letter and continuing from left to right
+     *  - two consecutive letters consisting of "A" followed by a letter that was not "A" were swapped
+     *  - letters were swapped at most once
+     */
+    public static String scrambleWord(String word) {
+        if (word.length() < 2) {
+            return word;
+        }
+        for (int i = 0; i < word.length() - 1; i++) {
+            String first = word.substring(i, i + 1);
+            String second = word.substring(i + 1, i + 2);
+            if (first.equals("A") && !second.equals("A")) {
+                word = word.substring(0, i) + second + first + (word.substring(i + 2));
+                i++;
+            }
+        }
+        return word;
+    }
 
-  /** Modifies wordList by replacing each word with its scrambled
-   *  version, removing any words that are unchanged as a result of scrambling.
-   *
-   *  @param wordList the list of words
-   *
-   *  Precondition: wordList contains only non-null objects
-   *  Postcondition:
-   *  - all words unchanged by scrambling have been removed from wordList
-   *  - each of the remaining words has been replaced by its scrambled version
-   *  - the relative ordering of the entries in wordList is the same as it was
-   *    before the method was called
-   */
-  public static ArrayList<String> scrambleOrRemove(ArrayList<String> wordList) {
-      /* to be implemented in part (b) */
-      return new ArrayList<String>();
-  }
+    /** Modifies wordList by replacing each word with its scrambled
+     *  version, removing any words that are unchanged as a result of scrambling.
+     *
+     *  @param wordList the list of words
+     *
+     *  Precondition: wordList contains only non-null objects
+     *  Postcondition:
+     *  - all words unchanged by scrambling have been removed from wordList
+     *  - each of the remaining words has been replaced by its scrambled version
+     *  - the relative ordering of the entries in wordList is the same as it was
+     *    before the method was called
+     */
+    public static ArrayList<String> scrambleOrRemove(ArrayList<String> wordList) {
+        for (int i = 0; i < wordList.size(); i++) {
+            String current = wordList.get(i);
+            String scrambled = scrambleWord(wordList.get(i));
+            if (current != scrambled) {
+                wordList.set(i, scrambled);
+            } else {
+                wordList.remove(i);
+                i--;
+            }
+            
+        }
+        return wordList;
+    }
 }
