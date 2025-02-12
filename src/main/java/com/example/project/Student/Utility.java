f0ddf10f90bbcfdbfca97c8263891c4ada557d67
3mma-n <148897839+3mma-n@users.noreply.github.com>
2025-02-12 17:19:26 +0000

    2/12

--- src/main/java/com/example/project/Student/Utility.java
+++ src/main/java/com/example/project/Student/Utility.java
@@ -6,19 +6,29 @@ public class Utility {
 
     public static ArrayList<Student> sortStudents(ArrayList<Student> list){
         for (int i = 0; i < list.size(); i++) {
-            for (int j = 0; j < list.size(); j++) {
-                int compareLast = list.get(i).getLastName().compareTo(list.get(j).getLastName());
-                int compareFirst = list.get(i).getFirsttName().compareTo(list.get(j).getFirstName());
-                int compareGpa = list.get(i).getGpa() - list.get(j).getGpa();
-                if (compareFirst != 0) {
-                    if (compareFirst > 0) {
-                        
+            for (int j = i; j < list.size(); j++) {
+                double compareLast = list.get(i).getLastName().compareTo(list.get(j).getLastName());
+                double compareFirst = list.get(i).getFirstName().compareTo(list.get(j).getFirstName());
+                double compareGPA = list.get(i).getGpa() - list.get(j).getGpa();
+                boolean later = false;
+                if (compareLast != 0) {
+                    if (compareLast > 0) {
+                        later = true;
                     }
+                } else if (compareFirst != 0) {
+                    if (compareFirst > 0) {
+                        later = true;
+                    }
+                } else {
+                    if (compareGPA < 0) {
+                        later = true;
+                    }
+                }
+                if ((later && i < j)) {
+                    list.add(i, list.remove(j));
                 }
             }
         }
         return list;
     }
-
-
 }
